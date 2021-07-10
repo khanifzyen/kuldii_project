6dff4a6a21eccfd158dd6b555fc2b475c7540e5d
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2021-07-11 06:51:27 +0700

    myList

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,38 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static List<Widget> myList = [
+    Container(
+      height: 200,
+      width: 100,
+      color: Colors.red,
+    ),
+    Container(
+      height: 200,
+      width: 100,
+      color: Colors.green,
+    ),
+    Container(
+      height: 200,
+      width: 100,
+      color: Colors.blue,
+    ),
+    Container(
+      height: 200,
+      width: 100,
+      color: Colors.yellow,
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("My ListView"),
         ),
-        body: ListView(
-          scrollDirection: Axis.horizontal,
-          children: [
-            Container(
-              height: 200,
-              width: 100,
-              color: Colors.red,
-            ),
-            Container(
-              height: 200,
-              width: 100,
-              color: Colors.green,
-            ),
-            Container(
-              height: 200,
-              width: 100,
-              color: Colors.blue,
-            ),
-            Container(
-              height: 200,
-              width: 100,
-              color: Colors.yellow,
-            )
-          ],
-        ),
+        body: ListView(children: myList),
       ),
     );
   }

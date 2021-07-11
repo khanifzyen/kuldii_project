3daa9b5414dc98470f969879d32d8f3a43a54398
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2021-07-11 12:19:46 +0700

    mapping list

--- lib/main.dart
+++ lib/main.dart
@@ -4,57 +4,167 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int counter = 50;
+  final List<Map<String, dynamic>> myList = [
+    {
+      "name": "Akhmad",
+      "age": 20,
+      "favColor": [
+        "Green",
+        "Red",
+        "Blue",
+        "Yellow",
+        "Green",
+        "Red",
+        "Blue",
+        "Yellow",
+      ],
+    },
+    {
+      "name": "Khanif",
+      "age": 23,
+      "favColor": [
+        "Purple",
+        "Orange",
+        "Black",
+        "Amber",
+      ],
+    },
+    {
+      "name": "Zyen",
+      "age": 25,
+      "favColor": [
+        "Matte",
+        "Orchid",
+        "Blue Ocean",
+        "Red Velvet",
+      ],
+    },
+    {
+      "name": "Akhmad",
+      "age": 20,
+      "favColor": [
+        "Green",
+        "Red",
+        "Blue",
+        "Yellow",
+        "Green",
+        "Red",
+        "Blue",
+        "Yellow",
+      ],
+    },
+    {
+      "name": "Khanif",
+      "age": 23,
+      "favColor": [
+        "Purple",
+        "Orange",
+        "Black",
+        "Amber",
+      ],
+    },
+    {
+      "name": "Zyen",
+      "age": 25,
+      "favColor": [
+        "Matte",
+        "Orchid",
+        "Blue Ocean",
+        "Red Velvet",
+      ],
+    },
+    {
+      "name": "Akhmad",
+      "age": 20,
+      "favColor": [
+        "Green",
+        "Red",
+        "Blue",
+        "Yellow",
+        "Green",
+        "Red",
+        "Blue",
+        "Yellow",
+      ],
+    },
+    {
+      "name": "Khanif",
+      "age": 23,
+      "favColor": [
+        "Purple",
+        "Orange",
+        "Black",
+        "Amber",
+      ],
+    },
+    {
+      "name": "Zyen",
+      "age": 25,
+      "favColor": [
+        "Matte",
+        "Orchid",
+        "Blue Ocean",
+        "Red Velvet",
+      ],
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Dynamic Apps",
+      title: "ID Apps",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Dynamic Apps"),
+          title: Text("ID Apps"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              counter.toString(),
-              style: const TextStyle(fontSize: 50),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                OutlinedButton(
-                  onPressed: () {
-                    if (counter != 1) {
-                      setState(() {
-                        counter--;
-                      });
-                    }
-                    //print(counter);
-                  },
-                  child: const Icon(Icons.remove),
+        body: ListView(
+          children: myList.map((data) {
+            final List<String> favColor = data["favColor"];
+
+            return Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(),
+                        const SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name : ${data['name']}"),
+                            Text("Age : ${data['age']}"),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: favColor.map((color) {
+                          return Container(
+                            child: Text(color),
+                            color: Colors.amber,
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 5,
+                              vertical: 5,
+                            ),
+                            padding: const EdgeInsets.all(5),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
                 ),
-                OutlinedButton(
-                  onPressed: () {
-                    setState(() {
-                      counter++;
-                    });
-                    //print(counter);
-                  },
-                  child: const Icon(Icons.add),
-                )
-              ],
-            ),
-          ],
+              ),
+            );
+          }).toList(),
         ),
       ),
     );

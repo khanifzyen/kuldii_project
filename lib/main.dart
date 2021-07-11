cfd51544e40d090a6db7da2039150b535eeb9fd4
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2021-07-11 15:05:17 +0700

    tabbar default

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "AppBar",
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.facebook),
-          title: const Text("My App Bar"),
-          actions: const [
-            Icon(Icons.search),
-            SizedBox(
-              width: 10,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Icon(Icons.facebook),
+            title: const Text("My App Bar"),
+            actions: const [
+              Icon(Icons.search),
+              SizedBox(
+                width: 10,
+              ),
+              Icon(Icons.add),
+              SizedBox(
+                width: 10,
+              ),
+            ],
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.add_a_photo),
+                  text: "Tab 1",
+                ),
+                Tab(
+                  icon: Icon(Icons.ac_unit_rounded),
+                  text: "Tab 2",
+                ),
+                Tab(
+                  icon: Icon(Icons.access_time),
+                  text: "Tab 3",
+                ),
+              ],
             ),
-            Icon(Icons.add),
-            SizedBox(
-              width: 10,
-            ),
-          ],
-        ),
-        body: const Center(
-          child: Text("Halo"),
+          ),
+          body: const Center(
+            child: Text("Halo"),
+          ),
         ),
       ),
     );

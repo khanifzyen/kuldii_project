c21e9d7fb2900fcb88bc8dc5c4a8c4be20592c3b
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2021-07-13 06:23:34 +0700

    dialog

---
+++ lib/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String data = "Belum ada input";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dialog"),
+      ),
+      body: Center(
+        child: Text(
+          data,
+          style: const TextStyle(fontSize: 25),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("telah diklik");
+          showDialog(
+            context: context,
+            builder: (builder) {
+              return AlertDialog(
+                title: const Text("Confirm"),
+                content: const Text("Are you sure to delete this item?"),
+                actions: [
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        data = "false";
+                      });
+                      print("klik no");
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("No"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        data = "true";
+                      });
+                      print("klik yes");
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Yes"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.delete),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,18 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-  final List<Container> myList = List.generate(
-      100,
-      (index) => Container(
-            height: 50,
-            width: 150,
-            color: Color.fromARGB(
-              255,
-              Random().nextInt(256),
-              Random().nextInt(256),
-              Random().nextInt(256),
-            ),
-          ));
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Grid View",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Grid View"),
-        ),
-        // body: GridView(
-        //   padding: const EdgeInsets.all(10),
-        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        //       crossAxisCount: 3,
-        //       crossAxisSpacing: 10,
-        //       mainAxisSpacing: 10,
-        //       childAspectRatio: 3 / 4),
-        //   children: myList,
-        // ),
-        body: GridView.count(
-          crossAxisCount: 3,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 3 / 4,
-          children: myList,
-        ),
-      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }

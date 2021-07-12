ed7f117c4fa3accc3b16f36ace58569d023d5945
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2021-07-12 13:59:51 +0700

    gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final TextEditingController myController = TextEditingController();
-
-  String hasil = "HASIL INPUT";
-
+  final List<Container> myList = List.generate(
+      100,
+      (index) => Container(
+            height: 50,
+            width: 150,
+            color: Color.fromARGB(
+              255,
+              Random().nextInt(256),
+              Random().nextInt(256),
+              Random().nextInt(256),
+            ),
+          ));
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Text Field",
+      title: "Grid View",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Text Field"),
+          title: const Text("Grid View"),
         ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextField(
-                  controller: myController,
-                  onChanged: (value) {
-                    print("onchange");
-                  },
-                  onSubmitted: (value) {
-                    print(value);
-                    setState(() {
-                      hasil = value;
-                      myController.clear();
-                    });
-                  },
-                  onEditingComplete: () {
-                    print("edit sukses");
-                    print(myController.text);
-                  },
-                ),
-                Text(hasil),
-              ],
-            ),
-          ),
+        // body: GridView(
+        //   padding: const EdgeInsets.all(10),
+        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //       crossAxisCount: 3,
+        //       crossAxisSpacing: 10,
+        //       mainAxisSpacing: 10,
+        //       childAspectRatio: 3 / 4),
+        //   children: myList,
+        // ),
+        body: GridView.count(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          childAspectRatio: 3 / 4,
+          children: myList,
         ),
       ),
     );

781633db3e24bd63840b17ccf5cb33a67a88fcae
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2021-07-12 11:08:13 +0700

    penggunaan textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController myController = TextEditingController();
+
+  String hasil = "HASIL INPUT";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: "Text Field",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Fitur Text Field"),
+          title: const Text("Text Field"),
         ),
-        body: const Center(
+        body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              autocorrect: false,
-              autofocus: true,
-              enableSuggestions: true,
-              enableInteractiveSelection: true,
-              enabled: true,
-              obscureText: true,
-              //obscuringCharacter: "*",
-              keyboardType: TextInputType.number,
-              readOnly: true,
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  controller: myController,
+                  onChanged: (value) {
+                    print("onchange");
+                  },
+                  onSubmitted: (value) {
+                    print(value);
+                    setState(() {
+                      hasil = value;
+                      myController.clear();
+                    });
+                  },
+                  onEditingComplete: () {
+                    print("edit sukses");
+                    print(myController.text);
+                  },
+                ),
+                Text(hasil),
+              ],
             ),
           ),
         ),

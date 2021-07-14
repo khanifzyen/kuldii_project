c19f82d363de8686e377c53f7a26bc3208c34728
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2021-07-14 08:16:48 +0700

    switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/my_drawer.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -18,22 +16,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool statusSwitch = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Drawer"),
+        title: Text("Switch"),
       ),
       body: Center(
-        child: Text(
-          "Home Page",
-          style: TextStyle(fontSize: 25),
+        child: Column(
+          children: [
+            Switch(
+              inactiveThumbColor: Colors.red,
+              activeColor: Colors.green,
+              activeTrackColor: Colors.green[200],
+              inactiveTrackColor: Colors.red[100],
+              value: statusSwitch,
+              onChanged: (value) {
+                setState(() {
+                  statusSwitch = !statusSwitch;
+                  print(statusSwitch);
+                });
+              },
+            ),
+            Text(
+              (statusSwitch == true) ? "Switch On" : "Switch Off",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
         ),
       ),
-      drawer: MyDrawer(),
     );
   }
 }
